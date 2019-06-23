16b180133db9e6f632a1dc324b70e064fb3642e0
Yue Yang <yueyang@Yues-MacBook-Pro.local>
2019-06-23 08:38:33 -0400

    Tried adding front-end

--- CSProjects/serp_data/app.py
+++ CSProjects/serp_data/app.py
@@ -1,12 +1,36 @@
 import sqlite3
-from flask import g, Flask, render_template
-app = Flask(__name__)
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table as dt
+import plotly
+import plotly.graph_objs as go
+import pandas as pd
 
 
+def run_query_withparms(sql):
+    conn = sqlite3.connect('serp_data.db')
+    df = pd.read_sql_query(sql , conn)
+  #  print(df)
+    return df
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    form = SearchForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        return redirect((url_for('search_results', query=form.search.data)))  # or what you want
-    return render_template('search.html', form=form)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.Label("Text Input"),
+    dcc.Input(id='text_in',value="MTL",type="text"),
+    html.Div(id='tab_container',children=[html.Div(dt.DataTable(id="tab_out"))])
+    ])
+
+
+@app.callback(Output("tab_container","children"), [Input("text_in","value")])
+
+def run_query(value):
+    sql2 = 'SELECT * FROM queries limit 10;'
+    return [html.Div([dt.DataTable(data=run_query_withparms(sql2))])]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
